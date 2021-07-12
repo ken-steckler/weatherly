ad7cf1029a827e300a73369332cfc500a363732c
Ken Steckler <steckler92ken@gmail.com>
2021-07-12 12:29:32 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
+import "./App.css";
+import { Dimmer, Loader } from "semantic-ui-react";
 
-function App() {
+const api = {
+  key: "ad58305515ce81da0e1c23a16629b3ab",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+export default function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+
+    fetchData();
+  }, [lat, long]);
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setData(result);
+
+          console.log(result);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="Wrapper">
+          <div className="searchbox">
+            <input
+              type="text"
+              className="searchbar"
+              placeholder="Search..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+        </div>
+        {typeof data.main != "undefined" ? (
+          <Weather weatherData={data} />
+        ) : (
+          <div>
+            <Dimmer active>
+              <Loader>Loading..</Loader>
+            </Dimmer>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
-
-export default App;
